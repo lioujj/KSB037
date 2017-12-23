ebc2bf77a118c15cc2824490b71fe849888daabf
lioujj <34760803+lioujj@users.noreply.github.com>
2017-12-23 15:54:16 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -15,7 +15,7 @@ namespace KSB037 {
     /**
     * 左右馬達同時反轉
     */
-    //% blockId="backward" block="小車後退|轉速(0~1023) %power"
+    //% blockId="backward" block="car backward|speed(0~1023) %power"
     //% blockGap=1  weight=80
     //% power.min=0 power.max=1023
     export function backward(power: number) {
@@ -27,7 +27,7 @@ namespace KSB037 {
     /**
     * 左馬達正轉，右馬達反轉
     */
-    //% blockId="rightward" block="小車右轉|轉速(0~1023) %power"
+    //% blockId="rightward" block="car rightward|speed(0~1023) %power"
     //% blockGap=1  weight=70
     //% power.min=0 power.max=1023
     export function rightward(power: number) {
@@ -39,7 +39,7 @@ namespace KSB037 {
     /**
     * 左馬達反轉，右馬達正轉
     */
-    //% blockId="leftward" block="小車左轉|轉速(0~1023) %power"
+    //% blockId="leftward" block="car leftward|speed(0~1023) %power"
     //% blockGap=20  weight=60
     //% power.min=0 power.max=1023
     export function leftward(power: number) {
@@ -48,28 +48,28 @@ namespace KSB037 {
         pins.analogWritePin(AnalogPin.P1, power)
         pins.analogWritePin(AnalogPin.P2, power)
     }
-    //% blockId="leftMotor_foreward" block="左馬達正轉|轉速(0~1023) %power"
+    //% blockId="leftMotor_foreward" block="left motor foreward|speed(0~1023) %power"
     //% blockGap=1  weight=30
     //% power.min=0 power.max=1023
     export function leftMotor_foreward(power: number) {
         pins.digitalWritePin(DigitalPin.P12, 1)
         pins.analogWritePin(AnalogPin.P2, power)
     }
-    //% blockId="leftMotor_backward" block="左馬達反轉|轉速(0~1023) %power"
+    //% blockId="leftMotor_backward" block="left motor backward|speed(0~1023) %power"
     //% blockGap=1  weight=20
     //% power.min=0 power.max=1023
     export function leftMotor_backward(power: number) {
         pins.digitalWritePin(DigitalPin.P12, 0)
         pins.analogWritePin(AnalogPin.P2, power)
     }
-    //% blockId="rightMotor_foreward" block="右馬達正轉|轉速(0~1023) %power"
+    //% blockId="rightMotor_foreward" block="right motor foreward|speed(0~1023) %power"
     //% blockGap=1  weight=50
     //% power.min=0 power.max=1023
     export function rightMotor_foreward(power: number) {
         pins.digitalWritePin(DigitalPin.P8, 1)
         pins.analogWritePin(AnalogPin.P1, power)
     }
-    //% blockId="rightMotor_backward" block="右馬達反轉|轉速(0~1023) %power"
+    //% blockId="rightMotor_backward" block="right motor backward|speed(0~1023) %power"
     //% blockGap=1 weight=40
     //% power.min=0 power.max=1023
     export function rightMotor_backward(power: number) {
@@ -79,7 +79,7 @@ namespace KSB037 {
     /**
     * 左右馬達同時停止轉動
     */
-    //% blockId="stop" block="小車停車"
+    //% blockId="stop" block="car stop"
     //% blockGap=20 weight=75
     export function stop() {
         pins.analogWritePin(AnalogPin.P1, 0)
